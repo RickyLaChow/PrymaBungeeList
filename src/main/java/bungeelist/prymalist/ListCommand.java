f20553710ed960e07b1d75205ed2b79a92f53b82
Rickylachow <rickylachow@gmail.com>
2022-06-11 22:16:04 +0200

    Improvements the testings

--- src/main/java/bungeelist/prymalist/ListCommand.java
+++ src/main/java/bungeelist/prymalist/ListCommand.java
@@ -9,6 +9,8 @@ import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
+import java.util.Map;
+
 
 public class ListCommand extends Command {
 
@@ -19,13 +21,19 @@ public class ListCommand extends Command {
     @Override
     public void execute(CommandSender sender, String[] args) {
 
-             ProxyServer.getInstance().getConsole().sendMessage("Test message");
+
+		Map<String, ServerInfo> test = ProxyServer.getInstance().getServersCopy();
+
+             ProxyServer.getInstance().getConsole().sendMessage("Array:" + test);
              ProxyServer.getInstance().broadcast(String.valueOf
                      (ProxyServer.getInstance().getPlayers()));
              ProxyServer.getInstance().broadcast(String.valueOf
                 (ProxyServer.getInstance().getPlayers().size()));
              ProxyServer.getInstance().broadcast(String.valueOf
                      (ProxyServer.getInstance().getServerInfo("dev").getPlayers()));
+		ProxyServer.getInstance().broadcast(String.valueOf
+			(ProxyServer.getInstance().getServerInfo("dev").getName()));
+
              ProxyServer.getInstance().broadcast(String.valueOf
                 ((long) ProxyServer.getInstance().getServerInfo("dev").getPlayers().size()));;
 
