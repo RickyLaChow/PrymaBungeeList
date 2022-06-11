a3c6639c2d6b5b949d5e6c84f6fbf5d3cad72cb1
Rickylachow <Riccardo5048@gmail.com>
2022-06-11 15:05:12 +0200

    Plugin base

---
+++ src/main/java/bungeelist/prymalist/Prymalist.java
@@ -0,0 +1,16 @@
+package bungeelist.prymalist;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Prymalist extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
