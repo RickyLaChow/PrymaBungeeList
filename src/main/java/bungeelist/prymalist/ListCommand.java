9c817b866df70ca923f9b3eaf118ff63890a6b89
Rickylachow <Riccardo5048@gmail.com>
2022-06-13 14:12:31 +0200

    feat: the command now can be execute only by console

--- src/main/java/bungeelist/prymalist/ListCommand.java
+++ src/main/java/bungeelist/prymalist/ListCommand.java
@@ -1,15 +1,12 @@
 package bungeelist.prymalist;
 
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-import java.util.Map;
+import java.io.Console;
+import java.net.Proxy;
 
 
 public class ListCommand extends Command {
@@ -20,13 +17,15 @@ public class ListCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (!(sender instanceof ProxiedPlayer)) {
             String[] Culetto = ProxyServer.getInstance().getServersCopy().keySet().toArray(new String[0]);
             System.out.println("Informazioni giocatori servers:");
-             for (int i = 0; i < ProxyServer.getInstance().getServersCopy().keySet().size(); i++) {
+            for (int i = 0; i < ProxyServer.getInstance().getServersCopy().keySet().size(); i++) {
                 System.out.println(Culetto[i] + ": " +
                         String.valueOf(ProxyServer.getInstance().getServerInfo(Culetto[i]).getPlayers().size()));
                 System.out.println("Lista Giocatori " + Culetto[i] + " " +
                         String.valueOf(ProxyServer.getInstance().getServerInfo(Culetto[i]).getPlayers()));
-             }
+            }
+        }
     }
 }
