30ecc88492eb2ff6cc903adb5b93bd5cb04d0ab6
Rickylachow <Riccardo5048@gmail.com>
2022-06-16 12:54:17 +0200

    feat: Added total players count

--- src/main/java/bungeelist/prymalist/ListCommand.java
+++ src/main/java/bungeelist/prymalist/ListCommand.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.scheduler.TaskScheduler;
 
 
 public class ListCommand extends Command {
@@ -12,17 +13,19 @@ public class ListCommand extends Command {
         super("list");
     }
 
+
     @Override
     public void execute(CommandSender sender, String[] args) {
         if (!(sender instanceof ProxiedPlayer)) {
-            String[] Culetto = ProxyServer.getInstance().getServersCopy().keySet().toArray(new String[0]);
+            String[] Servers = ProxyServer.getInstance().getServersCopy().keySet().toArray(new String[0]);
             System.out.println("Informazioni giocatori servers:");
-            for (int i = 0; i < ProxyServer.getInstance().getServersCopy().keySet().size(); i++) {
-                System.out.println(Culetto[i] + ": " +
-                        String.valueOf(ProxyServer.getInstance().getServerInfo(Culetto[i]).getPlayers().size()));
-                System.out.println("Lista Giocatori " + Culetto[i] + " " +
-                        String.valueOf(ProxyServer.getInstance().getServerInfo(Culetto[i]).getPlayers()));
+            for (int i = 0; i < ProxyServer.getInstance().getServersCopy().keySet().size(); i++){
+                System.out.println(Servers[i] + ": " +
+                        String.valueOf(ProxyServer.getInstance().getServerInfo(Servers[i]).getPlayers().size()));
+                System.out.println("Lista Giocatori " + Servers[i] + " " +
+                        String.valueOf(ProxyServer.getInstance().getServerInfo(Servers[i]).getPlayers()));
             }
+            System.out.println("Giocatori totali: " + ProxyServer.getInstance().getOnlineCount());
         }
     }
 }
